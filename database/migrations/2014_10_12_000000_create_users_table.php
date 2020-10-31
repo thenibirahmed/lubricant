6a1a1f9939e59322d524b72250abe725daf06613
nibirahmed9611 <nibirahmed9611@gmail.com>
2020-10-31 12:26:16 +0600

    Laravel Collective Form installed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,24 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
+class CreateUsersTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
+    public function up() {
+        Schema::create( 'users', function ( Blueprint $table ) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string( 'name' );
+            $table->string( 'email' )->unique();
+
+
+            $table->string( 'cell_no' );
+            $table->string( 'nid' );
+            $table->string( 'shop_name' )->nullable();
+            $table->string( 'trade_lisence' )->nullable();
+            $table->foreignId( 'shop_image' )->nullable();
+            $table->string( 'fathers_name' )->nullable();
+            $table->string( 'division' );
+            $table->string( 'district' );
+            $table->string( 'subdistrict' );
+
+            $table->foreignId( 'role_id' )->nullable();
+
+            $table->timestamp( 'email_verified_at' )->nullable();
+            $table->string( 'password' );
+
             $table->rememberToken();
             $table->timestamps();
-        });
+        } );
     }
 
     /**
@@ -29,8 +42,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        Schema::dropIfExists('users');
+    public function down() {
+        Schema::dropIfExists( 'users' );
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+{{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> --}}
+{{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> --}}
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> --}}
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -40,6 +43,72 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="fathers_name" class="col-md-4 col-form-label text-md-right">{{ __('Fathers Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="fathers_name" type="text" class="form-control @error('fathers_name') is-invalid @enderror" name="fathers_name" value="{{ old('fathers_name') }}">
+
+                                @error('fathers_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="cell_no" class="col-md-4 col-form-label text-md-right">{{ __('Cell No') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="cell_no" type="text" class="form-control @error('cell_no') is-invalid @enderror" name="cell_no" value="{{ old('cell_no') }}">
+
+                                @error('cell_no')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="nid" class="col-md-4 col-form-label text-md-right">{{ __('National Id (NID)') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="nid" type="text" class="form-control @error('nid') is-invalid @enderror" name="nid" value="{{ old('nid') }}">
+
+                                @error('nid')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+
+                            <div class="col-md-6">
+                                {{-- <input id="role" type="text" class="form-control @error('role') is-invalid @enderror" name="role" value="{{ old('role') }}"> --}}
+
+                                <select name="role" id="role" class="form-control">
+                                    <option value="dealer">Dealer</option>
+                                    <option value="shop_owner">Shop Owner</option>
+                                    <option value="service_center">Service Center</option>
+                                    <option value="sales_representative">Sales Representative</option>
+                                    <option value="divisional_manager">Divisional Manager</option>
+                                </select>
+
+                                @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -61,8 +130,24 @@
                             </div>
                         </div>
 
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <p>
+                                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                        Shop Owner/ Service Center form
+                                    </button>
+                                </p>
+                                <div class="collapse" id="collapseExample">
+                                    <div class="card card-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-6 text-right">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
@@ -74,4 +159,5 @@
         </div>
     </div>
 </div>
+
 @endsection
